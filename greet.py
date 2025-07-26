daf4fb31a481b04683253bfb24754f2b07e16b04
mounika123-alt <kmounika71@gmail.com>
2025-07-26 19:53:50 +0530

    Add files via upload

---
+++ greet.py
@@ -0,0 +1,5 @@
+#greet using firstname and last name
+first_name = input("Enter your first name:")
+last_name = input("Enter your last name:")
+# Greet the user
+print("Hello, " + first_name + " " + last_name + "! welcome to the python program")
